fd3edf882bb8afe2aebdab562fae0cea4eddb5ae
Yuval Fatal <92246164+YuvalFireFly@users.noreply.github.com>
2025-08-03 14:53:54 +0300

    Create main.tf

---
+++ production-server/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket         = "workflows-pre-ga-test-april-2024"
+    key            = "production-server.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-08a6efd148b1f7504"
+  instance_type = "t3.micro"
+  subnet_id     = "subnet-095804f2ff2be589b"
+  tags = {
+    Name = "production-web"
+  }
+}
