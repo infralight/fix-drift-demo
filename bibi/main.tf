bdf815f43260c2d2f33f7e789695e284c01d753c
Yuval Fatal <92246164+YuvalFireFly@users.noreply.github.com>
2025-08-28 12:43:01 +0300

    Update main.tf

--- bibi/main.tf
+++ bibi/main.tf
@@ -1,7 +1,7 @@
 module "ha_secured_instances" {
   source = "github.com/infralight/well-architected-env//modules/ha_secured_instances"
 
-  cluster_size  = 3
+  cluster_size  = 4
   instance_name = "bibi"
   instance_type = "SMALL"
 }
